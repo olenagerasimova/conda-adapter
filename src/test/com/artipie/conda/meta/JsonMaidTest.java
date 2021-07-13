2a179891716bee01bc3ed2f9aa81d6f190121370
Alena <olena.gerasimova@gmail.com>
2021-07-13 15:18:08 +0400

    feature: added JsonMaid and implementation
    Added JsonMaid and implementation that uses com.fasterxml.jackson to read/write json.
    
    Issue: #5
    PR: #8

---
+++ src/test/com/artipie/conda/meta/JsonMaidTest.java
@@ -0,0 +1,37 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/conda-adapter/LICENSE
+ */
+package com.artipie.conda.meta;
+
+import com.artipie.asto.test.TestResource;
+import com.fasterxml.jackson.core.JsonFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link JsonMaid.Jackson}.
+ * @since 0.1
+ */
+class JsonMaidTest {
+
+    @Test
+    void doesNothingIfChecksumsAreEmpty() throws IOException {
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        final JsonFactory factory = new JsonFactory();
+        final TestResource resource = new TestResource("repodata.json");
+        new JsonMaid.Jackson(
+            factory.createGenerator(stream).useDefaultPrettyPrinter(),
+            factory.createParser(resource.asInputStream())
+        ).clean(Collections.emptySet());
+        MatcherAssert.assertThat(
+            stream.toByteArray(),
+            new IsEqual<>(resource.asBytes())
+        );
+    }
+
+}
