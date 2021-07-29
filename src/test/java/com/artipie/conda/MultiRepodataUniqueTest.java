a9175f11d2e2a7dcbfd1f6e1a2fcd8e1d452b45b
olenagerasimova <olena.gereasimova@gmail.com>
2021-07-29 11:08:43 +0300

    Added more tests for merging metadata

--- src/test/java/com/artipie/conda/MultiRepodataUniqueTest.java
+++ src/test/java/com/artipie/conda/MultiRepodataUniqueTest.java
@@ -12,33 +12,77 @@ import java.nio.charset.StandardCharsets;
 import org.cactoos.list.ListOf;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 /**
  * Test for {@link MultiRepodata.Unique}.
  * @since 0.3
  */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 class MultiRepodataUniqueTest {
 
-    @Test
-    void mergesPackages() throws UnsupportedEncodingException, JSONException {
-        final ByteArrayOutputStream res = new ByteArrayOutputStream();
+    @ParameterizedTest
+    @CsvSource({
+        "m2p_first_1.json,m2p_second_1.json,m2p_res_1.json",
+        "m2p_first_2.json,m2p_second_1.json,m2p_res_2.json",
+        "m2p_first_1.json,m2p_second_3.json,m2p_res_3.json",
+        "m2p_first_4.json,m2p_second_4.json,m2p_res_4.json",
+        "m2p_first_5.json,m2p_second_1.json,m2p_second_1.json",
+        "m2p_second_1.json,m2p_first_5.json,m2p_second_1.json"
+    })
+    void mergesTwoPackages(final String first, final String second, final String res)
+        throws UnsupportedEncodingException, JSONException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
         new MultiRepodata.Unique().merge(
-            new ListOf<InputStream>(
-                new TestResource("MultiRepodataUniqueTest/mergesPackages_input1.json")
-                    .asInputStream(),
-                new TestResource("MultiRepodataUniqueTest/mergesPackages_input2.json")
-                    .asInputStream()
-            ),
-            res
+            new ListOf<InputStream>(this.resourceStream(first), this.resourceStream(second)), out
         );
         JSONAssert.assertEquals(
-            res.toString(StandardCharsets.UTF_8.name()),
+            out.toString(StandardCharsets.UTF_8.name()),
             new String(
-                new TestResource("MultiRepodataUniqueTest/mergesPackages_res.json").asBytes(),
+                new TestResource(String.format("MultiRepodataUniqueTest/%s", res)).asBytes(),
                 StandardCharsets.UTF_8
             ),
             true
         );
+    }
+
+    @Test
+    void doesNothingIfOnePackageProvided() throws UnsupportedEncodingException, JSONException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        new MultiRepodata.Unique().merge(
+            new ListOf<InputStream>(new TestResource("repodata.json").asInputStream()), out
+        );
+        JSONAssert.assertEquals(
+            out.toString(StandardCharsets.UTF_8.name()),
+            new String(new TestResource("repodata.json").asBytes(), StandardCharsets.UTF_8),
+            true
+        );
+    }
+
+    @Test
+    void excludesDuplicates() throws UnsupportedEncodingException, JSONException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        new MultiRepodata.Unique().merge(
+            new ListOf<InputStream>(
+                this.resourceStream("exludesDupl_input1.json"),
+                this.resourceStream("exludesDupl_input2.json"),
+                this.resourceStream("exludesDupl_input3.json")
+            ), out
+        );
+        JSONAssert.assertEquals(
+            out.toString(StandardCharsets.UTF_8.name()),
+            new String(
+                new TestResource("MultiRepodataUniqueTest/exludesDupl_res.json").asBytes(),
+                StandardCharsets.UTF_8
+            ),
+            true
+        );
+    }
+
+    private InputStream resourceStream(final String name) {
+        return new TestResource(String.format("MultiRepodataUniqueTest/%s", name))
+            .asInputStream();
     }
 }
