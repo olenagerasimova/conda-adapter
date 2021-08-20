80f9512ba5906e62063a21d6670e4a0dedc007a5
olenagerasimova <olena.gereasimova@gmail.com>
2021-08-20 16:58:33 +0300

    Added class to update `repodata.json` from storage

---
+++ src/main/java/com/artipie/conda/asto/AstoMergedJson.java
@@ -0,0 +1,105 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/conda-adapter/LICENSE
+ */
+package com.artipie.conda.asto;
+
+import com.artipie.asto.ArtipieIOException;
+import com.artipie.asto.Content;
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.asto.misc.UncheckedIOConsumer;
+import com.artipie.asto.misc.UncheckedIOFunc;
+import com.artipie.asto.misc.UncheckedIOScalar;
+import com.artipie.conda.meta.MergedJson;
+import com.fasterxml.jackson.core.JsonFactory;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import javax.json.JsonObject;
+import org.cqfn.rio.Buffers;
+import org.cqfn.rio.WriteGreed;
+import org.cqfn.rio.stream.ReactiveInputStream;
+import org.cqfn.rio.stream.ReactiveOutputStream;
+
+/**
+ * Asto merged json adds packages metadata to repodata index, reading and writing to/from
+ * abstract storage.
+ * @since 0.4
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+public final class AstoMergedJson {
+
+    /**
+     * Abstract storage.
+     */
+    private final Storage asto;
+
+    /**
+     * Repodata file key.
+     */
+    private final Key key;
+
+    /**
+     * Ctor.
+     * @param asto Abstract storage
+     * @param key Repodata file key
+     */
+    public AstoMergedJson(final Storage asto, final Key key) {
+        this.asto = asto;
+        this.key = key;
+    }
+
+    /**
+     * Merges or adds provided new packages items into repodata.json.
+     * @param items Items to merge
+     * @return Completable operation
+     */
+    public CompletionStage<Void> merge(final Map<String, JsonObject> items) {
+        return this.asto.exists(this.key).thenCompose(
+            exists -> {
+                final CompletionStage<Void> future;
+                Optional<PipedInputStream> pis = Optional.empty();
+                try (PipedOutputStream outout = new PipedOutputStream()) {
+                    if (exists) {
+                        pis = Optional.of(new PipedInputStream());
+                        final PipedInputStream val = pis.get();
+                        future = this.asto.value(this.key).thenCompose(
+                            input -> new ReactiveOutputStream(
+                                new UncheckedIOScalar<>(() -> new PipedOutputStream(val))
+                                    .value()
+                            ).write(input, WriteGreed.SYSTEM));
+                    } else {
+                        future = CompletableFuture.allOf();
+                        pis = Optional.empty();
+                    }
+                    future.thenCompose(
+                        new UncheckedIOFunc<>(
+                            nothing -> this.asto.save(
+                                this.key,
+                                new Content.From(
+                                    new ReactiveInputStream(new PipedInputStream(outout))
+                                        .read(Buffers.Standard.K16)
+                                )
+                            )
+                        )
+                    );
+                    final JsonFactory factory = new JsonFactory();
+                    new MergedJson.Jackson(
+                        factory.createGenerator(outout),
+                        pis.map(new UncheckedIOFunc<>(factory::createParser))
+                    ).merge(items);
+                } catch (final IOException err) {
+                    throw new ArtipieIOException(err);
+                } finally {
+                    pis.ifPresent(new UncheckedIOConsumer<>(PipedInputStream::close));
+                }
+                return future;
+            }
+        );
+    }
+}
